30d1b8cdb07fc8f6719afe8d709bb8dc571263dc
linclip <linclip@gmail.com>
2019-03-18 14:54:54 +0900

    first commit

--- main.ts
+++ main.ts
@@ -1,7 +1,121 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=100 color=#0fbc11 icon="\u0023" block="Hexadecimal"
+
+namespace hexadecimal {
+
+    const numFontHex = [
+        ["11", "11", "11", "11", "11"],   //0
+        ["01", "01", "01", "01", "01"],   //1
+        ["11", "01", "11", "10", "11"],   //2
+        ["11", "01", "11", "01", "11"],   //3
+        ["10", "10", "11", "01", "01"],   //4
+        ["11", "10", "11", "01", "11"],   //5
+        ["11", "10", "11", "11", "11"],   //6
+        ["11", "01", "01", "01", "01"],   //7
+        ["11", "11", "00", "11", "11"],   //8
+        ["11", "11", "11", "01", "11"],   //9
+        ["00", "11", "01", "11", "11"],   //a
+        ["00", "10", "10", "11", "11"],   //b
+        ["00", "00", "11", "10", "11"],   //c
+        ["00", "01", "01", "11", "11"],   //d
+        ["00", "11", "11", "10", "11"],   //e
+        ["00", "01", "10", "11", "10"]   //f
+    ]
+
+    /**
+     * show number in hexadecimal
+     * @param v number, eg: 0
+     */
+    //% blockId=showHex block="Show Hex  %v"
+
+    export function showHex(v: number): void {
+        basic.clearScreen()
+        if (v < 0) {
+            basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+            return
+        }
+        let n2 = 0
+        n2 = Math.floor(v % 256 / 16)
+        let n1 = Math.abs(Math.floor(v % 16))
+
+        for (let y = 0; y < 5; y++) {
+            if (n2 != 0 || v > 255) {
+                for (let x = 0; x < 2; x++) {
+                    if ((numFontHex[n2][y]).substr(x, 1) == "1") {
+                        led.plot(x, y)
+                    }
+                }
+            }
+            for (let z = 3; z < 5; z++) {
+                if ((numFontHex[n1][y]).substr(z - 3, 1) == "1") {
+                    led.plot(z, y)
+                }
+            }
+        }
+    }
+
+    const numFont = [
+        ["11", "11", "11", "11", "11"],   //0
+        ["01", "01", "01", "01", "01"],   //1
+        ["11", "01", "11", "10", "11"],   //2
+        ["11", "01", "11", "01", "11"],   //3
+        ["10", "10", "11", "01", "01"],   //4
+        ["11", "10", "11", "01", "11"],   //5
+        ["11", "10", "11", "11", "11"],   //6
+        ["11", "01", "01", "01", "01"],   //7
+        ["11", "11", "00", "11", "11"],   //8
+        ["11", "11", "11", "01", "11"],   //9
+        ["00", "00", "11", "00", "00"]    //-
+    ]
+
+    /**
+     * show number in two digits
+     * @param v Numer, eg: 0
+     */
+    //% blockId=show2digits block="two digits  %v"
+    export function show2digits(v: number): void {
+        basic.clearScreen()
+        if (v < -9) {
+            basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+            return
+        }
+        let n2 = 0
+        if (v < 0) {
+            n2 = 10
+        } else {
+            n2 = Math.floor(v % 100 / 10)
+        }
+        let n1 = Math.abs(Math.floor(v % 10))
+
+        for (let y = 0; y < 5; y++) {
+            if (n2 != 0 || v > 99) {
+                for (let x = 0; x < 2; x++) {
+                    if ((numFont[n2][y]).substr(x, 1) == "1") {
+                        led.plot(x, y)
+                    }
+                }
+            }
+            for (let z = 3; z < 5; z++) {
+                if ((numFont[n1][y]).substr(z - 3, 1) == "1") {
+                    led.plot(z, y)
+                }
+            }
+        }
+
+
+    }
+
+
+
+} 
--- test.ts
+++ test.ts
@@ -1,1 +1,73 @@
-// tests go here; this will not be compiled when this package is used as a library
+let hundred = 0
+let loopFlag = 0
+let counter = 0
+input.onButtonPressed(Button.AB, function () {
+    if (loopFlag == 0) {
+        loopFlag = 1
+    } else {
+        loopFlag = 0
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    loopFlag = 0
+    counter += -1
+    if (counter < 0) {
+        counter = 0
+    }
+    hexadecimal.showHex(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    loopFlag = 0
+    counter += 1
+    if (counter > 255) {
+        counter = 255
+    }
+    hexadecimal.showHex(counter)
+})
+input.onGesture(Gesture.Shake, function () {
+    counter = 0
+    hexadecimal.showHex(counter)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    counter += -16
+    if (counter < 0) {
+        counter = 0
+    }
+    hexadecimal.showHex(counter)
+})
+input.onPinPressed(TouchPin.P0, function () {
+    loopFlag = 0
+    basic.showLeds(`
+        # # . . .
+        . # . . #
+        # # . . #
+        # . . # #
+        # # . # #
+        `)
+    hundred = 0
+    if (counter > 99) {
+        hundred = Math.floor(counter / 100)
+        hexadecimal.show2digits(hundred)
+        basic.pause(1000)
+    }
+    hexadecimal.show2digits(counter)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    counter += 16
+    if (counter > 255) {
+        counter = 255
+    }
+    hexadecimal.showHex(counter)
+})
+counter = 0
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (loopFlag == 1) {
+        counter += 1
+        if (counter > 255) {
+            counter = 0
+        }
+        hexadecimal.showHex(counter)
+        basic.pause(100)
+    }
+})

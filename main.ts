e518abf8694ab407f9a19a6becfba972165a3c9d
linclip <linclip@gmail.com>
2019-03-18 18:41:13 +0900

    update font

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 
 namespace hexadecimal {
 
-    const numFontHex = [
+    const numFont = [
         ["11", "11", "11", "11", "11"],   //0
         ["01", "01", "01", "01", "01"],   //1
         ["11", "01", "11", "10", "11"],   //2
@@ -18,7 +18,8 @@ namespace hexadecimal {
         ["00", "00", "11", "10", "11"],   //c
         ["00", "01", "01", "11", "11"],   //d
         ["00", "11", "11", "10", "11"],   //e
-        ["00", "01", "10", "11", "10"]   //f
+        ["00", "01", "10", "11", "10"],   //f
+        ["00", "00", "11", "00", "00"]    //-
     ]
 
     /**
@@ -46,19 +47,19 @@ namespace hexadecimal {
         for (let y = 0; y < 5; y++) {
             if (n2 != 0 || v > 255) {
                 for (let x = 0; x < 2; x++) {
-                    if ((numFontHex[n2][y]).substr(x, 1) == "1") {
+                    if ((numFont[n2][y]).substr(x, 1) == "1") {
                         led.plot(x, y)
                     }
                 }
             }
             for (let z = 3; z < 5; z++) {
-                if ((numFontHex[n1][y]).substr(z - 3, 1) == "1") {
+                if ((numFont[n1][y]).substr(z - 3, 1) == "1") {
                     led.plot(z, y)
                 }
             }
         }
     }
-
+/*
     const numFont = [
         ["11", "11", "11", "11", "11"],   //0
         ["01", "01", "01", "01", "01"],   //1
@@ -72,12 +73,12 @@ namespace hexadecimal {
         ["11", "11", "11", "01", "11"],   //9
         ["00", "00", "11", "00", "00"]    //-
     ]
-
+*/
     /**
      * show number in decimal (two digits)
      * @param v Numer, eg: 0
      */
-    //% blockId=show2digits block="two digits  %v"
+    //% blockId=showNumberDecimal block="two digits  %v"
     export function showNumberDecimal(v: number): void {
         basic.clearScreen()
         if (v < -9) {
@@ -92,7 +93,7 @@ namespace hexadecimal {
         }
         let n2 = 0
         if (v < 0) {
-            n2 = 10
+            n2 = 16
         } else {
             n2 = Math.floor(v % 100 / 10)
         }
